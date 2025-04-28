bb15d44edbe7dd98402de31e4a234115d39cd389
EnzoCaetano015 <peracioenzo@gmail.com>
2025-04-28 20:10:41 -0300

    ajustes

--- Api-PHP/Routes/rotas.php
+++ Api-PHP/Routes/rotas.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/../Controllers/ExemploController.php';
 
-$base = '/Api-PHP'; 
-$uri = str_replace($base, '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$scriptName = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+$uri = str_replace($scriptName, '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $method = $_SERVER['REQUEST_METHOD'];
 
 $exemploController = new ExemploController();
@@ -16,4 +16,3 @@ if ($uri == '/api' && $method == 'GET') {
     echo json_encode(["erro" => "Rota não encontrada"]);
 }
 ?>
-
