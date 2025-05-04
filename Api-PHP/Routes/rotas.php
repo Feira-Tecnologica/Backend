a319c3e464720e511b7cbc0426b38bf2c94b5e2e
GabrielCaspirro <gabriel.gcd08@gmail.com>
2025-05-04 12:11:42 -0300

    Adicionei o controller de login e passei a rota pro routes

--- Api-PHP/Routes/rotas.php
+++ Api-PHP/Routes/rotas.php
@@ -1,16 +1,22 @@
 <?php
 require_once __DIR__ . '/../Controllers/ExemploController.php';
+require_once __DIR__ . '/../Controllers/LoginController.php';
 
 $scriptName = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
 $uri = str_replace($scriptName, '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $method = $_SERVER['REQUEST_METHOD'];
 
 $exemploController = new ExemploController();
+$loginController = new LoginController();
 
 if ($uri == '/api' && $method == 'GET') {
     $exemploController->index();
 } elseif ($uri == '/api/usuarios' && $method == 'GET') {
     $exemploController->listarUsuarios();
+} elseif ($uri == '/api/login/aluno' && $method == 'POST') {
+    $loginController->VerificaLoginAluno();
+} elseif ($uri == '/api/login/professor' && $method == 'POST') {
+    $loginController->VerificaLoginProfessor();
 } else {
     http_response_code(404);
     echo json_encode(["erro" => "Rota não encontrada"]);
