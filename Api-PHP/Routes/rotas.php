76b458bd252c91f3743a635c10a81232a65f176f
GabrielCaspirro <gabriel.gcd08@gmail.com>
2025-05-04 13:53:48 -0300

    Adição do controller de email, para código de confirmação

--- Api-PHP/Routes/rotas.php
+++ Api-PHP/Routes/rotas.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../Controllers/ExemploController.php';
 require_once __DIR__ . '/../Controllers/LoginController.php';
+require_once __DIR__ . '/../Controllers/EmailController.php';
 
 $scriptName = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
 $uri = str_replace($scriptName, '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
@@ -8,6 +9,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 $exemploController = new ExemploController();
 $loginController = new LoginController();
+$emailController = new EmailController();
 
 if ($uri == '/api' && $method == 'GET') {
     $exemploController->index();
@@ -17,6 +19,8 @@ if ($uri == '/api' && $method == 'GET') {
     $loginController->VerificaLoginAluno();
 } elseif ($uri == '/api/login/professor' && $method == 'POST') {
     $loginController->VerificaLoginProfessor();
+} elseif ($uri == '/api/codigoconfirmacao' && $method == 'POST') {
+    $emailController->enviarCodigoConfirmacao();
 } else {
     http_response_code(404);
     echo json_encode(["erro" => "Rota não encontrada"]);
